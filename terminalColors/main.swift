82174d6e98190e41f6bba4f45a5a3c55f22aa5aa
João Amaral <jamaral@pasonatquila.com>
2015-06-29 10:11:14 +0900

    Initial Commit

---
+++ terminalColors/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  terminalColors
+//
+//  Created by João Amaral on 6/29/15Mo.
+//  Copyright © 2015 João Amaral. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
